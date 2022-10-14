9669724aff30b26a9b840ce376a151a2779c1bbe
gbortola <gabriele.bortolato@cern.ch>
2022-10-14 10:54:22 +0200

    empty branch

--- hls4ml/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from hls4ml.model.graph import ModelGraph, HLSConfig
-
-try:
-    from hls4ml.model import profiling
-    __profiling_enabled__ = True
-except ImportError:
-    __profiling_enabled__ = False
